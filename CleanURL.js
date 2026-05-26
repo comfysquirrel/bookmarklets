d07a7fcb84ee698d1e2d4d9301e9e811337394d5
comfysquirrel <jdh11235@gmail.com>
2026-05-26 17:02:52 -0500

    Add Ebay, Etsy, Facebook Marketplace

--- CleanURL.js
+++ CleanURL.js
@@ -1,14 +1,22 @@
 javascript:(() => {
     const currentURL = window.location.href;
-    const domain = currentURL.match(/https?:\/\/[a-z0-1-.]*(?<domain>((amazon)|(youtube)|(etsy))\.[^\/]*)/i).groups.domain;
+    const domain = currentURL.match(/https?:\/\/[a-z0-1-.]*(?<domain>((amazon)|(ebay)|(etsy)|(facebook)|(youtube))\.[^\/]*)/i).groups.domain;
     let cleanURL;
     if (domain.includes("amazon")) {
         const id = currentURL.match(/(\/(dp|gp\/product(\/glance)?|gp\/aw\/d|exec\/obidos\/asin)\/)(?<id>[a-z0-9]*)/i).groups.id;
         cleanURL = `https://${domain}/dp/${id}`;
     }
+    if (domain.includes("ebay")) {
+        const id = currentURL.match(/(\/itm\/)(?<id>[0-9]*)/i).groups.id;
+        cleanURL = `https://${domain}/itm/${id}`;
+    }
     if (domain.includes("etsy")) {
         const id = currentURL.match(/(\/listing\/)(?<id>[0-9]*)/i).groups.id;
         cleanURL = `https://${domain}/listing/${id}`;
+    }
+    if (domain.includes("facebook")) {
+        const id = currentURL.match(/(\/marketplace\/item\/)(?<id>[0-9]*)/i).groups.id;
+        cleanURL = `https://${domain}/marketplace/item/${id}`;
     }
     if (domain.includes("youtube")) {
         const id = currentURL.match(/(\?v=)(?<id>[a-z0-9-_]*)/i).groups.id;
