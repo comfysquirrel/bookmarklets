705a257a04b4dc692d87572aa79a3d656e5713a4
comfysquirrel <jdh11235@gmail.com>
2026-05-26 15:45:10 -0500

    Add Amazon URL

--- CleanURL.js
+++ CleanURL.js
@@ -1,3 +1,13 @@
-// Amazon URL
+javascript:(() => {
+    const currentURL = window.location.href;
+    const domain = currentURL.match(/https?:\/\/[a-z0-1.]*(?<domain>((amazon)|(youtube))\.[^\/]*)/i).groups.domain;
+    let cleanURL;
+    if (domain.includes("amazon")) {
+        const id = currentURL.match(/(\/(dp|gp\/product(\/glance)?|gp\/aw\/d|exec\/obidos\/asin)\/)(?<id>[a-z0-9]*)/i).groups.id;
+        cleanURL = `https://${domain}/dp/${id}`;
+    }
+    if (domain.includes("youtube")) {
 
-// YouTube URL
+    }
+    navigator.clipboard.writeText(cleanURL);
+})();
