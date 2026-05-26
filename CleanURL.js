8b0c124898455e4bbe9a458b9ec26755110f0e75
comfysquirrel <jdh11235@gmail.com>
2026-05-26 16:44:08 -0500

    More correct domain name format

--- CleanURL.js
+++ CleanURL.js
@@ -1,6 +1,6 @@
 javascript:(() => {
     const currentURL = window.location.href;
-    const domain = currentURL.match(/https?:\/\/[a-z0-1.]*(?<domain>((amazon)|(youtube))\.[^\/]*)/i).groups.domain;
+    const domain = currentURL.match(/https?:\/\/[a-z0-1-.]*(?<domain>((amazon)|(youtube))\.[^\/]*)/i).groups.domain;
     let cleanURL;
     if (domain.includes("amazon")) {
         const id = currentURL.match(/(\/(dp|gp\/product(\/glance)?|gp\/aw\/d|exec\/obidos\/asin)\/)(?<id>[a-z0-9]*)/i).groups.id;
