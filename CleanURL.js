48eeb43159d37f1733a016478fe863920c5dc56d
comfysquirrel <jdh11235@gmail.com>
2026-05-26 16:52:17 -0500

    Add Etsy URLs

--- CleanURL.js
+++ CleanURL.js
@@ -1,10 +1,14 @@
 javascript:(() => {
     const currentURL = window.location.href;
-    const domain = currentURL.match(/https?:\/\/[a-z0-1-.]*(?<domain>((amazon)|(youtube))\.[^\/]*)/i).groups.domain;
+    const domain = currentURL.match(/https?:\/\/[a-z0-1-.]*(?<domain>((amazon)|(youtube)|(etsy))\.[^\/]*)/i).groups.domain;
     let cleanURL;
     if (domain.includes("amazon")) {
         const id = currentURL.match(/(\/(dp|gp\/product(\/glance)?|gp\/aw\/d|exec\/obidos\/asin)\/)(?<id>[a-z0-9]*)/i).groups.id;
         cleanURL = `https://${domain}/dp/${id}`;
+    }
+    if (domain.includes("etsy")) {
+        const id = currentURL.match(/(\/listing\/)(?<id>[0-9]*)/i).groups.id;
+        cleanURL = `https://${domain}/listing/${id}`;
     }
     if (domain.includes("youtube")) {
         const id = currentURL.match(/(\?v=)(?<id>[a-z0-9-_]*)/i).groups.id;
